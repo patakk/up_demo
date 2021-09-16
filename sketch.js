ac9cf0683665248c9e47179d3c4d826590c29daf
patakk <paolo.ceric@gmail.com>
2021-09-16 04:07:47 +0200

    good

--- sketch.js
+++ sketch.js
@@ -13,6 +13,11 @@ var particles = [];
 
 var ttime = "DAY";
 
+var frameBuffer = [];
+var bufferSize = 4;
+
+var ghost = false;
+
 function pad(n, width, z) {
     z = z || '0';
     n = n + '';
@@ -22,20 +27,127 @@ function pad(n, width, z) {
 function loadAnimation(path, ind1, ind2) {
     var paths = [];
     for(var k = ind1; k <= ind2; k++){
-        var npath = path + String(pad(k, 4)) + ".png";
+        //var npath = path + String(pad(k, 4)) + ".png";
+        if(path == "data/frames_lowres/4_anim_aktivacija_druga/4_ani_" && k == 21)
+            continue;
+        var npath = path + String(k) + ".png";
         paths.push(npath);
     }
     frames = [];
     for(var k = 0; k < paths.length; k++){
         frames.push(loadImage(paths[k]));
     }
-    console.log("loaded")
+    console.log("loaded", path)
     return frames
 }
 
+var ulazak;
+var kruzenje;
+var akt1;
+var akt2;
+
+var akt;
+
+var clicked = false;
+
+class Akt {
+    constructor(prefix, fs, fe) {
+        this.prefix = prefix;
+        this.fs = fs;
+        this.fe = fe;
+        this.frames = loadAnimation(this.prefix, this.fs, this.fe);
+        this.numFrames = this.frames.length;
+        this.currentFrameIdx = 0;
+        // this.currentFrame = this.frames[this.currentFrameIdx];
+        this.nextAkt = this;
+    }
+
+    setNextAkt(nextAkt){
+        this.nextAkt = nextAkt;
+    }
+    
+    getNextAkt(){
+        if(this.nextAkt !== kruzenje)
+            this.nextAkt.currentFrameIdx = 0;
+        this.nextAkt.previous = this;
+        return this.nextAkt;
+    }
+
+    advance(){
+        this.previous = this;
+        var idx = this.currentFrameIdx;
+        this.currentFrameIdx++;
+        if(this.currentFrameIdx == this.numFrames && this === kruzenje)
+            this.currentFrameIdx = 0;
+        frameBuffer.push(this.frames[idx]);
+        if(frameBuffer.length == bufferSize+1)
+            frameBuffer.shift();
+        return this.frames[idx];
+    }
+
+    getFrame(idx){
+        if(idx === null){
+            frameBuffer.push(this.frames[this.currentFrameIdx]);
+            if(frameBuffer.length == bufferSize+1)
+                frameBuffer.shift();
+            return this.frames[this.currentFrameIdx];
+        }
+        else{
+            frameBuffer.push(this.frames[idx]);
+            if(frameBuffer.length == bufferSize+1)
+                frameBuffer.shift();
+            return this.frames[idx];
+        }
+    }
+}
+
+
+class Pro {
+    constructor(particles) {
+        this.numFrames = 14*4;
+        this.currentFrameIdx = 0;
+        // this.currentFrame = this.frames[this.currentFrameIdx];
+        this.nextAkt = this;
+        this.particles = particles;
+    }
+
+    setNextAkt(nextAkt){
+        this.nextAkt = nextAkt;
+    }
+    
+    getNextAkt(){
+        if(this.nextAkt !== kruzenje)
+            this.nextAkt.currentFrameIdx = 0;
+        this.nextAkt.previous = this;
+        return this.nextAkt;
+    }
+
+    advance(){
+        this.previous = this;
+        var idx = this.currentFrameIdx;
+        this.currentFrameIdx++;
+        if(this.currentFrameIdx == this.numFrames && this === kruzenje)
+            this.currentFrameIdx = 0;
+        this.particles[1].display(24);
+        if(frameCount % 2 == 0){
+            image(this.parent.getFrame(this.parent.currentFrameIdx), 0, 0, width, height);
+            filter(ERODE);
+        }
+        else{
+            image(this.parent.getFrame(this.parent.currentFrameIdx), 0, 0, width, height);
+        }
+    }
+
+
+}
+
+
 function preload() {
-    anims.push(loadAnimation("data/anim4/", 1, 67));
-    anims.push(loadAnimation("data/anim5/", 1, 24));
+    ulazak = new Akt("data/frames_lowres/1_anim_ulazak/1_ani_", 1, 60);
+    kruzenje = new Akt("data/frames_lowres/2_anim_kruzenje/2_ani_", 1, 66);
+    akt1 = new Akt("data/frames_lowres/3_anim_aktivacija_prva/3_ani_", 1, 34);
+    akt2 = new Akt("data/frames_lowres/4_anim_aktivacija_druga/4_ani_", 1, 67);
+    console.log("LOADED");
 }
 
 function setup() {
@@ -55,13 +167,23 @@ function setup() {
     var pts1 = [0.67344498, 0.42640693, 0.64712919, 0.35714286, 0.7284689, 0.3030303, 0.81220096, 0.45454545];
     var pts2 = [0.51076555, 0.59307359, 0.6076555, 0.57142857, 0.73564593, 0.72943723, 0.46889952, 0.82467532];
     var pts3 = [0.53110048, 0.30952381, 0.45574163, 0.32467532, 0.4007177, 0.25541126, 0.55023923, 0.23160173];
-
+    
     particles.push(new Particle(0, pts0));
     particles.push(new Particle(1, pts1));
     particles.push(new Particle(2, pts2));
     particles.push(new Particle(3, pts3));
 
+    akt3 = new Pro(particles);
+
     frameRate(14);
+
+    akt = ulazak;
+
+    ulazak.setNextAkt(kruzenje);
+    akt1.setNextAkt(kruzenje);
+    akt2.setNextAkt(kruzenje);
+    akt3.setNextAkt(kruzenje);
+    console.log("STARTED");
 }
 
 function draw() {
@@ -69,27 +191,79 @@ function draw() {
     noStroke();
     
     fill(0);
+
+    if(akt.currentFrameIdx == akt.numFrames && akt !== kruzenje){
+        akt = akt.getNextAkt();
+    }
+
+    if(akt === kruzenje && (akt.previous === kruzenje || akt.previous === ulazak) && akt.currentFrameIdx == 0 && clicked){
+        akt = akt1;
+        akt.currentFrameIdx = 0;
+        clicked = false;
+    }
+
+    if(akt === kruzenje && (akt.previous === kruzenje || akt.previous === ulazak) && akt.currentFrameIdx == 25 && clicked){
+        akt = akt2;
+        akt.currentFrameIdx = 0;
+        clicked = false;
+    }
+    
+    if(akt === kruzenje && (akt.previous === kruzenje || akt.previous === ulazak) && akt.currentFrameIdx == 39 && clicked){
+        akt3.parent = akt;
+        akt = akt3;
+        akt.currentFrameIdx = 0;
+        clicked = false;
+        for (var k = 0; k < akt.particles.length; k++) {
+            akt.particles[k].displayFlag = true;
+            akt.particles[k].age = round(random(-20, 0));
+        }
+        akt.numFrames = 100; // ovaj 100 mora bit varijabla za svaku proceduralnu animaciju
+    }
+
+    if(akt !== akt3){
+        var ss1 = Math.round(2*Math.sin(frameCount*0.1));
+        var ss2 = Math.round(-1-1*Math.sin(frameCount*0.1));
+        //image(akt.getFrame((akt.currentFrameIdx+akt.numFrames-ss2)%akt.numFrames), 0, 0, width, height);
+        /*if(frameBuffer.length == bufferSize){
+            tint(255, 180 + 0*(0.5 + 0.5*sin(frameCount*0.2)));
+            image(frameBuffer[bufferSize-2], 0, 0, width, height);
+            tint(255, 100 + 0*(0.5 + 0.5*sin(frameCount*0.2)));
+            image(frameBuffer[bufferSize-3], 0, 0, width, height);
+        }*/
+        if(frameBuffer.length == bufferSize && ghost == true){
+            tint(255, 100, 200, random(50, 220) + 0*(0.5 + 0.5*sin(frameCount*0.2)));
+            image(frameBuffer[bufferSize-1], 0, 0, width, height);
+            if(random() < -0.5)
+                filter(INVERT);
+            tint(255, 200, 100, random(50, 220) + 0*(0.5 + 0.5*sin(frameCount*0.2)));
+            image(frameBuffer[bufferSize-2], 0, 0, width, height);
+            if(random() < -0.5)
+                filter(INVERT);
+            //filter(BLUR, 1);
+        }
+        tint(255, 255);
+        image(akt.advance(), 0, 0, width, height);
+    }
+    else{
+        akt.advance();
+    }
+
+    /*
     if (playing || frameIdx != 0){
         image(anims[anim_ind][frameIdx], -width/2*0, -height/2*0, width, height);
-        //console.log(anims[anim_ind][frameIdx]);
-        //frameIdx = (frameIdx + 1) % anims[anim_ind].getLastFrame();
         frameIdx = frameIdx + 1;
         if (frameIdx >= anims[anim_ind].length){
             playing = false;
             frameIdx = 0;
         }
     }
-    /*if(frameIdx == 0 && changeAnim){
-        anim_ind = (anim_ind + 1) % anims.length;
-        changeAnim = false;
-    }*/
 
     for(var k = 0; k < particles.length*0+1; k++){
         var gridnum = 30;
         if(k == 2)
             gridnum = 18;
         particles[2].display(gridnum);
-    }
+    }*/
 
     //rotate(radians(+12));
     //applyMatrix(1, 0, shear_factor, 1, 0, 0);
@@ -136,8 +310,10 @@ function keyPressed() {
         anim_ind = 3;
         playing = true;
     }
+    if (keyCode == 48) {
+        ghost = !ghost;
+    }
 
-    console.log(anim_ind);
     /*if(keyCode == 32){
         playing = !playing;
     }
@@ -147,11 +323,11 @@ function keyPressed() {
 }
 
 function mouseClicked() {
-
-    for (var k = 0; k < particles.length; k++) {
-        particles[k].displayFlag = true;
-        particles[k].age = round(random(-20, 0));
-    }
+    clicked = true;
+    //for (var k = 0; k < particles.length; k++) {
+    //    particles[k].displayFlag = true;
+    //    particles[k].age = round(random(-20, 0));
+    //}
 
     print(mouseX, mouseY);
 }
@@ -221,10 +397,10 @@ class Particle {
                 //ellipse(xx + random(-1, +1), yy + random(-1, +1), sx, sy);
 
                 if (ttime == "DAY") {
-                    stroke(40, 200);
+                    stroke(0);
                 }
                 else {
-                    stroke(80, 200);
+                    stroke(0);
                 }
                 strokeWeight(2);
 
